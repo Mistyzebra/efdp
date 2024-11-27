7c8c452af14139e7c5259e56bc76214e2ab6b41b
z5bra <z5bra@protonmail.com>
2024-11-27 14:11:20 +0800

    update

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 mailu(based on 2024-06)
 
 gophish
+
+
+```bash
+# 需要安装docker及openssl
+usage:
+chmod +x install.sh
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -12,8 +12,14 @@ if ! command -v docker &> /dev/null; then
   exit 1
 fi
 
+# 检查OpenSSL是否安装
+if ! command -v openssl &> /dev/null; then
+  echo "OpenSSL未安装，请先安装OpenSSL。"
+  exit 1
+fi
+
 # 获取用户输入并验证
-read -p "请输入服务器公网IP: " server_ip
+read -p "请输入服务器公网IP(或0.0.0.0): " server_ip
 if [[ ! $server_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   echo "无效的IP地址。"
   exit 1
@@ -26,9 +32,7 @@ if [[ ! $domain_name =~ ^[a-zA-Z0-9.-]+$ ]]; then
 fi
 
 read -p "请输入邮件服务器管理员密码: " password
-echo
 read -p "请输入GoPhish服务器管理员密码: " gppassword
-echo
 
 # 检查配置文件是否存在
 if [ ! -f docker-compose.yml ]; then
@@ -51,61 +55,91 @@ if [ ! -f gophish_config.json ]; then
   exit 1
 fi
 
-# 处理证书选项
-echo "请选择证书选项："
-echo "1. 使用自定义证书"
-echo "2. 生成自签名证书"
-read -p "请输入选项 (1 或 2): " cert_option
-
-if [ "$cert_option" -eq 1 ]; then
-  read -p "请输入自定义证书路径 (crt或pem格式): " cert_path
-  read -p "请输入自定义密钥路径 (key或pem格式): " key_path
-
-  if [ ! -f "$cert_path" ] || [ ! -f "$key_path" ]; then
-    echo "证书或密钥文件不存在。"
-    exit 1
-  fi
-
-elif [ "$cert_option" -eq 2 ]; then
-  # 检查OpenSSL是否安装
-  if ! command -v openssl &> /dev/null; then
-    echo "OpenSSL未安装，请先安装OpenSSL。"
-    exit 1
-  fi
-else
-  echo "无效的选项。"
-  exit 1
-fi
 
 # 所有检查通过后，开始执行操作
 
 # 创建目录结构
-echo "创建项目目录 /opt/efdp"
+echo "创建项目目录 /opt/efdp.."
 mkdir -p /opt/efdp/mailu/certs
 mkdir -p /opt/efdp/gophish/certs
 
 # 替换配置文件内容
+echo "配置文件修改.."
 sed -i "s/serverip/$server_ip/g" docker-compose.yml
 sed -i "s/gppassword/$gppassword/g" docker-compose.yml
 sed -i "s/example.com/$domain_name/g" mailu.env
 sed -i "s/yourpassword/$password/g" mailu.env
 
-# 处理证书
-if [ "$cert_option" -eq 1 ]; then
-  cp "$cert_path" /opt/efdp/mailu/certs/cert.pem
-  cp "$key_path" /opt/efdp/mailu/certs/key.pem
-  cp "$cert_path" /opt/efdp/gophish/certs/server.crt
-  cp "$key_path" /opt/efdp/gophish/certs/server.key
-elif [ "$cert_option" -eq 2 ]; then
+# 处理邮服证书
+echo "处理邮件服务器证书.."
+echo "请选择邮件服务器证书选项："
+echo "1. 使用自定义证书(efdp.example.com)"
+echo "2. 生成自签名证书(efdp.example.com)"
+read -p "请输入选项 (1 或 2): " mailcert_option
+
+if [ "$mailcert_option" -eq 1 ]; then
+  read -p "请输入自定义证书路径 (crt或pem格式): " mailcert_path
+  read -p "请输入自定义密钥路径 (key或pem格式): " mailkey_path
+
+  if [ ! -f "$mailcert_path" ] || [ ! -f "$mailkey_path" ]; then
+    echo "证书或密钥文件不存在。"
+    exit 1
+  fi
+  cp "$mailcert_path" /opt/efdp/mailu/certs/cert.pem
+  cp "$mailkey_path" /opt/efdp/mailu/certs/key.pem
+
+elif [ "$mailcert_option" -eq 2 ]; then
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /opt/efdp/mailu/certs/key.pem \
     -out /opt/efdp/mailu/certs/cert.pem \
     -subj "/CN=efdp.$domain_name"
-
-  cp /opt/efdp/mailu/certs/cert.pem /opt/efdp/gophish/certs/server.crt
-  cp /opt/efdp/mailu/certs/key.pem /opt/efdp/gophish/certs/server.key
-  echo "已生成efdp.$domain_name 域名证书"
+    
+  echo "已生成efdp.$domain_name 邮件服务器域名证书"
+else
+  echo "无效的选项。"
+  exit 1
 fi
+
+## 处理gophish证书
+echo "处理gophish服务器证书.."
+echo "gophish钓鱼页是否使用HTTPS?"
+read -p "(Y/N):" gptls
+
+if [ "$gptls" == "Y" ]; then
+  echo "钓鱼页使用HTTPS协议"
+  echo "请选择邮件服务器证书选项："
+  echo "1. 使用自定义证书"
+  echo "2. 生成自签名证书"
+  read -p "请输入选项 (1 或 2): " gpcert_option
+  if [ "$gpcert_option" -eq 1 ]; then
+    read -p "请输入自定义证书路径 (crt或pem格式): " gpcert_path
+    read -p "请输入自定义密钥路径 (key或pem格式): " gpkey_path
+
+    if [ ! -f "$gpcert_path" ] || [ ! -f "$gpkey_path" ]; then
+      echo "证书或密钥文件不存在。"
+      exit 1
+    fi
+    cp "$gpcert_path" /opt/efdp/gophish/certs/server.crt
+    cp "$gpkey_path" /opt/efdp/gophish/certs/server.key  
+  elif [ "$gpcert_option" -eq 2 ]; then
+    read -p "请钓鱼页域名: " gpdomain
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /opt/efdp/gophish/certs/server.key \
+    -out /opt/efdp/gophish/certs/server.crt \
+    -subj "/CN=$domain_name"
+    
+    echo "已生成$domain_name 邮件服务器域名证书"
+  else
+    echo "无效的选项。"
+    exit 1
+  fi
+elif [ "$gptls" == "N" ]; then
+  echo "钓鱼页不使用HTTPS协议"
+  sed -i 's/\"use_tls\": true/\"use_tls\": false/g' mailu.env
+else
+  echo "无效输入，请键入 Y 或 N"
+fi
+
 
 # 复制配置文件到目标目录
 cp docker-compose.yml /opt/efdp/docker-compose.yml
@@ -117,9 +151,9 @@ chmod -R 666 /opt/efdp/mailu/
 chmod -R 666 /opt/efdp/gophish/
 
 echo "安装完成。"
-echo "- 请在防火墙上开启以下端口："
-echo "8088,4433,25,465,587,110,995,143,993,4190,3333,80,443,8080"
 echo "- 在/opt/efdp目录下使用docker compose up -d启动。"
 echo "- mailu邮件服务器后台为8088/4433(tls)端口，gophish后台为3333端口，钓鱼页面为80/443端口"
-echo "请通过efdp.$domain_name 访问"
+echo "  请通过efdp.$domain_name 访问"
+echo "- 请在防火墙上开启以下端口："
+echo "  8088,4433,25,465,587,110,995,143,993,4190,3333,80,443,8080"
 echo "- *请确保25端口开放能给收发邮箱"
